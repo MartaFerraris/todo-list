2e0493168e84b7c241fb5618a665fa2a9c2fc77f
Marta Ferraris <141126985+MartaFerraris@users.noreply.github.com>
2024-06-25 17:38:24 +0200

    add delete and create component

--- src/components/Todoitems.jsx
+++ src/components/Todoitems.jsx
@@ -3,14 +3,14 @@ import tick from "../asset/tick.png"
 import no_tick from "../asset/no_tick.png"
 import delete_icon from "../asset/delete_icon.png"
 
-const TodoItems = ({text}) => { 
+const TodoItems = ({text, id, isComplete, deleteTodo}) => { 
     return (
         <div className="flex items-center my-3 gap-2">
             <div className="flex flex-1 items-center cursor-pointer">
                 <img className="w-7 bg-orange-600 rounded-full" src={tick} alt=""/>
                 <p className="text-slate-700 ml-4 text-[17px]">{text}</p>
             </div>
-            <img src={delete_icon} alt="" className="w-3.5 cursor-pointer" />
+            <img onClick={() => {deleteTodo(id)}} src={delete_icon} alt="" className="w-3.5 cursor-pointer" />
         </div>
 
     )
